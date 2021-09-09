856c117a85ad8faabde0a167309bbae45b19b2fb
Ousmane Sall <osall@univ-thies.sn>
2021-09-09 17:57:40 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projetNG01UVS';
+  title = 'Bienvenue sur mon composant principal';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      AccueilComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
