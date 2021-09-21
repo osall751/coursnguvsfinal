5ec449c3c325e841a578a1d849a0e613e9a68a9f
Ousmane SALL <osall@univ-thies.sn>
2021-09-21 09:14:57 +0000

    Version 21/09/2021 09:15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenue sur mon composant principal';
+  title = "Bienvenue l'application web du cours de Web Mobile";
 }
