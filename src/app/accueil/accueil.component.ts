983c206b32d65c895ef786ca43f2e2bc708e7d9a
Ousmane SALL <osall@univ-thies.sn>
2021-09-21 09:00:37 +0000

    Version 21/09/2021 09:00

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -6,14 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
+  userName = "osall@univ-thies.sn";
+  pwd = "passer123";
+  maDate = new Date();
+  imgSrc = "https://images.seneweb.com/dynamic/modules/news/images/gen/fb/46037cd575b285de551947e6d16838e2fe35b146.jpg"
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * valider
+   */
+  public valider() {
+    if (this.userName === "osall@univ-thies.sn") {
+      alert("Login et mdp saisies OK !!!")
+    } else {
+      alert("Login et mdp saisies KO !!!");
+    }
+  }
 }
-function add(a: number, b: string){
-  return a+b;
+
+function add(a: number, b: string) {
+  return a + b;
 };
- 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       AccueilComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
