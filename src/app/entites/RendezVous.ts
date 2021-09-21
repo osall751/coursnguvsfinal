e60a3d93ae36b5ee6bced5278e530955bbe7b53a
Ousmane SALL <osall@univ-thies.sn>
2021-09-21 11:30:04 +0000

    Version 21/09/2021 11:30

---
+++ src/app/entites/RendezVous.ts
@@ -0,0 +1,19 @@
+export class RendezVous {
+  constructor(private _id: number, private _description: string,
+    private _lieu: string,
+    private _date: Date) { }
+
+  public get id(): number {
+    return this._id;
+  }
+  public get description(): string {
+    return this._description;
+  }
+
+  public get lieu(): string {
+    return this._lieu;
+  }
+  public get date(): Date {
+    return this._date;
+  }
+}
--- src/app/rendezVous/rendezVous.component.ts
+++ src/app/rendezVous/rendezVous.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RendezVous } from '../entites/RendezVous';
 
 @Component({
   selector: 'app-rendezVous',
@@ -8,7 +9,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RendezVousComponent implements OnInit {
   nomUtilisateur: string | null="";
-
+  nombre=0;
+  rvs=[
+    new RendezVous(1, "Cours Angular","UVS",new Date("05/15/2022")),
+    new RendezVous(2, "Cours IONIC","UGB",new Date("06/25/2022")),
+    new RendezVous(3, "Cours PYTHON","UIDT",new Date("07/06/2022")),
+    new RendezVous(4, "Cours Visite","UASZ",new Date("05/15/2022")),
+    new RendezVous(5, "Cours Spring Boot","USSEIN",new Date("05/05/2022")),
+    new RendezVous(6, "Cours Pen Test","UAM",new Date("06/05/2022")),
+    new RendezVous(7, "Cours Ethycal Hacking","UVS",new Date("05/05/2022")),
+    new RendezVous(8, "Cours Audit","UIDT",new Date("08/05/2022")),
+    new RendezVous(9, "Cours Archi","UGB",new Date("12/05/2022")),
+    new RendezVous(10, "Cours Dev Web","SUPDECO",new Date("11/05/2022")),
+    new RendezVous(11, "Cours Santé Sport","UVS",new Date("10/05/2022")),
+  ]
   constructor(private routeActive: ActivatedRoute) { }
 
   ngOnInit() {
